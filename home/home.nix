0aef74b0261d8be2ca1251f9a349a9629c7d6dcf
Juan Jesus <juanjesusalbarran@gmail.com>
2025-11-01 19:45:31 +0100

    Add shared home-manager config

--- home/home.nix
+++ home/home.nix
@@ -1,20 +1,5 @@
 # home/home.nix
-{ config, pkgs, kukenan, system, ... }:
-
+{ ... }:
 {
-  home-manager.users.juan = {
-    imports = [
-      ./modules/common.nix
-      ./modules/ghostty
-      ./modules/hyprland
-      ./modules/waybar
-      ./modules/kanshi
-    ];
-
-    home = { 
-      username = "juan";
-      homeDirectory = "/home/juan";
-      stateVersion = "25.05";
-    };
-  };
+  home-manager.users.juan = import ./home-config.nix;
 }
