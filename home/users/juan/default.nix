598e96907528678eabf3b4903589284e5d7dfe7f
Juan Jesus <juanjesusalbarran@gmail.com>
2025-12-15 20:53:14 +0100

    Added fastfetch, added pictures of jacko, added multi-user configuration

---
+++ home/users/juan/default.nix
@@ -0,0 +1,28 @@
+# home/users/juan/default.nix
+{...}: {
+  imports = [
+    ./packages.nix
+    ./programs.nix
+    ./../../modules/ghostty
+    ./../../modules/hyprland
+    ./../../modules/waybar
+    ./../../modules/kanshi
+    ./../../modules/extras
+    ./../../modules/tmux
+    ./../../modules/starship
+    ./../../modules/fastfetch
+  ];
+
+  home = {
+    username = "juan";
+    homeDirectory = "/home/juan";
+    stateVersion = "25.05";
+  };
+  # Ensures the Home Manager-built Hyprland session is used by the Display Manager
+  # and sources the HM-configured environment variables and services.
+  xsession.enable = true;
+
+  # Also ensure your hyprland module (./../../modules/hyprland)
+  # explicitly configures the Home Manager-specific settings:
+  # programs.hyprland.enable = true; # (if you want Home Manager to manage the configuration file)
+}
---
+++ home/users/juan/programs.nix
@@ -0,0 +1,31 @@
+# home/users/juan/programs.nix
+{
+  programs = {
+    git = {
+      enable = true;
+      userName = "Juan Jesus";
+      userEmail = "juanjesusalbarran@gmial.com";
+    };
+
+    bat.enable = true;
+
+    bash = {
+      enable = true;
+      shellAliases = {
+        vim = "nvim-base";
+        cat = "bat";
+        wvim = "nvim-web";
+        jvim = "nvim-java";
+      };
+      initExtra = ''
+        fastfetch
+      '';
+    };
+    yazi = {
+      enable = true;
+    };
+    lazygit = {
+      enable = true;
+    };
+  };
+}
