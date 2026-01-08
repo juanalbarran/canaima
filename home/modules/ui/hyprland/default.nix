fc83941f4b2e066b456a57e94dd0356156e3ed70
Juan Jesus <juanjesusalbarran@gmail.com>
2026-01-08 21:16:11 +0100

    Configured waybar in hyprland

--- home/modules/ui/hyprland/default.nix
+++ home/modules/ui/hyprland/default.nix
@@ -1,11 +1,20 @@
 # home/modules/hyprland/default.nix
-{
+{pkgs, ...}: {
+  wayland.windowManager.hyprland = {
+    enable = true;
+    extraConfig = builtins.readFile ./hyprland.conf;
+  };
+
   xdg.configFile = {
-    "hypr/hyprland.conf".source = ./hyprland.conf;
     "hypr/hyprland-monitors.conf".source = ./hyprland-monitors.conf;
     "hypr/hyprland-programs.conf".source = ./hyprland-programs.conf;
     "hypr/hyprland-autostart.conf".source = ./hyprland-autostart.conf;
     "hypr/hyprland-keybinds.conf".source = ./hyprland-keybinds.conf;
     "hypr/hyprland-workspaces.conf".source = ./hyprland-workspaces.conf;
   };
+
+  home.packages = with pkgs; [
+    grim
+    slurp
+  ];
 }
--- home/modules/ui/hyprlock/default.nix
+++ home/modules/ui/hyprlock/default.nix
@@ -1,4 +1,5 @@
 # home/modules/hyprlock/default.nix
 {
+  programs.hyprlock.enable = true;
   xdg.configFile."hypr/hyprlock.conf".source = ./hyprlock.conf;
 }
