89bec081bd5b4cb16de5c10747d796cb3db26ca8
Juan Jesus <juanjesusalbarran@gmail.com>
2026-01-11 21:33:44 +0100

    Added tridactyl
    Added tridactyl on firefox

--- home/modules/browsers/firefox/default.nix
+++ home/modules/browsers/firefox/default.nix
@@ -12,4 +12,17 @@
   home.file = {
     ".mozilla/native-messaging-hosts/tridactyl.json".source = "${pkgs.tridactyl-native}/lib/mozilla/native-messaging-hosts/tridactyl.json";
   };
+  xdg.configFile."tridactyl/tridactylrc".text = ''
+    " Bind Next/Prev tab to Ctrl+Shift+[ and ]
+    bind <C-Shift-[> tabprev
+    bind <C-Shift-]> tabnext
+
+    " Alternative: If you prefer Alt+Shift
+    bind <A-Shift-[> tabprev
+    bind <A-Shift-]> tabnext
+
+    " Unbind standard keys if they conflict (Optional)
+    " unbind J
+    " unbind K
+  '';
 }
