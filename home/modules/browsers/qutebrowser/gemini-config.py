87eecd35faeb31396630a66fe1f5a1cc31856ce9
Juan Albarran <juan.albarran@nixs.com>
2026-01-13 18:30:23 +0100

    Sway changes
    Cleaning bindings in sway
    Made qutebroser the main browser for AI and surfing

---
+++ home/modules/browsers/qutebrowser/gemini-config.py
@@ -0,0 +1,50 @@
+# home/modules/browsers/qutebrowser/gemini-config.py
+
+import subprocess
+from qutebrowser.api import interceptor
+
+# --- 1. MINIMAL UI ---
+config.load_autoconfig(False)
+c.statusbar.show = 'never'
+c.tabs.show = 'never'
+c.scrolling.bar = 'never'
+
+config.bind('<Ctrl-d>', 'fake-key <PgDown>', mode='normal')
+config.bind('<Ctrl-u>', 'fake-key <PgUp>', mode='normal')
+
+config.bind('<Ctrl-d>', 'fake-key <PgDown>', mode='insert')
+config.bind('<Ctrl-u>', 'fake-key <PgUp>', mode='insert')
+
+# --- 2. LOCK NAVIGATION (The Failsafe Way) ---
+# Instead of unbinding (which crashes if the key isn't there),
+# we bind the keys to 'nop', which means "do nothing".
+# This works 100% of the time.
+
+keys_to_disable = ['o', 'O', 'go', 'gO', 'xo', 'xO', 'w', 'B']
+
+for key in keys_to_disable:
+    config.bind(key, 'nop')
+
+# --- 3. REDIRECT EXTERNAL LINKS ---
+def filter_traffic(info: interceptor.Request):
+    if info.resource_type != interceptor.ResourceType.main_frame:
+        return
+
+    url = info.request_url
+    host = url.host()
+    
+    allowed_hosts = [
+        "gemini.google.com",
+        "accounts.google.com",
+        "myaccount.google.com",
+        "gds.google.com"
+    ]
+
+    if any(allowed in host for allowed in allowed_hosts):
+        return 
+
+    info.block()
+    subprocess.Popen(['qutebrowser', url.toString()])
+
+interceptor.register(filter_traffic)
+
