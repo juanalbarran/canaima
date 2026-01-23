84d5fc420bc35c78a6c292ab0f45d1f4d1abe86e
Juan Albarran <juan.albarran@nixs.com>
2026-01-23 20:42:42 +0100

    Themes
    Implementing dark and light themes

---
+++ home/modules/ui/themes/dark.nix
@@ -0,0 +1,31 @@
+# home/modules/ui/themes/dark.nix
+{
+  slug = "dark";
+  name = "dark";
+  author = "Juan Jesus Albarran Rodriguez";
+  palette = {
+    base00 = "#131314"; # Background (Very Dark Grey)
+    base01 = "#32302f"; # Lighter Background (Status bars)
+    base02 = "#a9b1d6"; # Selection Background
+    base03 = "#665c54"; # Comments / Dimmed Text
+    base04 = "#bdae93"; # Dark Foreground
+    base05 = "#ffffff"; # Main Text (The cream color in your terminal)
+    base06 = "#ddc7a1"; # Light Foreground
+    base07 = "#fbf1c7"; # Light Background
+    base08 = "#ea6962"; # Red      (Matches your 'user' icon)
+    base09 = "#e78a4e"; # Orange
+    base0A = "#d8a657"; # Yellow   (Matches your 'uptime' icon)
+    base0B = "#a9b665"; # Green    (Matches your 'shell' icon)
+    base0C = "#89b482"; # Aqua/Cyan (Matches your 'network' icon)
+    base0D = "#7aa2f7"; # Blue     (Matches your 'distro' icon)
+    base0E = "#d3869b"; # Purple   (Matches your 'kernel' icon)
+    base0F = "#bd6f3e"; # Brown
+  };
+}
+# Important colosrs that i like
+# base00 = "#131314"; # Background
+# base02 = "#a9b1d6"; # Inactive borders
+# base05 = "#ffffff"; # Main Text
+# base08 = "#ea6962"; # Color for warnings
+# base0D = "#7aa2f7"; # Focused workspace / active borders
+
