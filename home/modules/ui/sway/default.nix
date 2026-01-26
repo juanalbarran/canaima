b1511faed9c63907b5f7744c7ff705911fea3663
Juan Albarran <juan.albarran@nixs.com>
2026-01-26 15:48:54 +0100

    Sway
    Putiing the whole sway home-manager configuration under config, so the
    isNixOS flag can work

--- home/modules/ui/sway/default.nix
+++ home/modules/ui/sway/default.nix
@@ -16,36 +16,38 @@
       description = "Set to true if running on NixOS, false for Ubuntu/other";
     };
   };
-  wayland.windowManager.sway = {
-    # Set the package null instead of pkgs.sway, to use the sway installed by apt
-    # in the Ubuntu machine
-    package =
-      if config.host.isNixOS
-      then pkgs.sway
-      else null;
-    checkConfig = true;
-    config = {
-      extraConfig = builtins.readFile ./config;
+  config = {
+    wayland.windowManager.sway = {
+      # Set the package null instead of pkgs.sway, to use the sway installed by apt
+      # in the Ubuntu machine
+      package =
+        if config.host.isNixOS
+        then pkgs.sway
+        else null;
+      checkConfig = true;
+      config = {
+        extraConfig = builtins.readFile ./config;
+      };
     };
-  };
-  xdg.configFile = {
-    "sway/config".source = ./config;
-    "sway/variables.conf".source = ./variables.conf;
-    "sway/autostart.conf".source = ./autostart.conf;
-    "sway/bindings.conf".source = ./bindings.conf;
-    "sway/monitors.conf".source = ./monitors.conf;
-    "sway/lock_config".source = ./swaylock.conf;
-    "sway/rules.conf".source = ./rules.conf;
-  };
-  home = {
-    packages = with pkgs; [
-      grim
-      slurp
-      wtype
-      wev
-    ];
-    activation.createScreenshotsDir = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      mkdir -p $HOME/Pictures/screenshots
-    '';
+    xdg.configFile = {
+      "sway/config".source = ./config;
+      "sway/variables.conf".source = ./variables.conf;
+      "sway/autostart.conf".source = ./autostart.conf;
+      "sway/bindings.conf".source = ./bindings.conf;
+      "sway/monitors.conf".source = ./monitors.conf;
+      "sway/lock_config".source = ./swaylock.conf;
+      "sway/rules.conf".source = ./rules.conf;
+    };
+    home = {
+      packages = with pkgs; [
+        grim
+        slurp
+        wtype
+        wev
+      ];
+      activation.createScreenshotsDir = lib.hm.dag.entryAfter ["writeBoundary"] ''
+        mkdir -p $HOME/Pictures/screenshots
+      '';
+    };
   };
 }
