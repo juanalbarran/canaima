ecfdb16781465b9879e2dd39cd2339a88caccc4c
Juan Jesus Albarran Rodriguez <juanjesusalbarran@gmail.com>
2026-01-27 18:46:05 +0100

    Wallpaper
    Change wallpaper script for a simpler one
    Put it in a service

---
+++ home/modules/ui/wallpapers/README.md
@@ -0,0 +1,7 @@
+# Wallpaper switcher
+
+This will iterate the wallpapers every 5 mins.
+The wallpapers are stored in `~/Pictures/Wallpapers/`
+For `1920x1080` or `normal` screens the directory is `~/Pictures/Wallpapers/normal`
+For `2560x1080` or `wide` screens the directory is `~/Pictures/Wallpapers/wide`
+For `dark` mode the directory is `~/Pictures/Wallpapers/normal/dark`
---
+++ home/modules/ui/wallpapers/scripts/wallpaper.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# configuration
+cache_dir="$HOME/.cache/style"
+state_file="$cache_dir/mode"
+base_wallpaper_dir="$HOME/Pictures/Wallpapers"
+
+# logic
+
+# identify the current mode (light/dark)
+current_mode=$(cat "$state_file" 2>/dev/null || echo "dark")
+echo "The current mode is: $current_mode mode"
+# identififying current screen resolution
+resolution="$(screen-resolution)"
+
+# now we can get the wallpaper_dir
+wallpaper_dir="$base_wallpaper_dir/$resolution/$current_mode/"
+
+# select a random wallpaper
+selected_wallpaper=$(find -L "$wallpaper_dir" -type f 2>/dev/null | shuf -n 1)
+
+if [ -z "$selected_wallpaper" ]; then
+  echo "Error: No wallpapers found in $wallpaper_dir"
+  exit 1
+fi
+
+# set the wallpaper
+swaymsg "output * bg '$selected_wallpaper' fill"
+
