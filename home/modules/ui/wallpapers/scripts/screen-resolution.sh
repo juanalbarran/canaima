bdc60c8d980886975a61d94452221da71e80c045
Juan Jesus Albarran Rodriguez <juanjesusalbarran@gmail.com>
2026-01-29 14:09:28 +0100

    Wallpapers
    Added ultrawide wallpapres
    Improved the script to determine, wide and ultrawide screens

--- home/modules/ui/wallpapers/scripts/screen-resolution.sh
+++ home/modules/ui/wallpapers/scripts/screen-resolution.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 # we get the dimentions using swaymsg
-dimentions=$(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | "\(.rect.width) \(.rect.height)"')
+dimensions=$(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | "\(.rect.width) \(.rect.height)"')
 
 # we separate the values
-read -r width height <<< "$dimentions"
+read -r width height <<< "$dimensions"
 
 # we calculate the ratio
 ratio=$(( width * 100 / height ))
 
-if [ "$ratio" -gt 200 ]; then
+if [ "$ratio" -gt 230 ]; then
+  screen_type="ultrawide"
+elif [ "$ratio" -ge 170 ]; then
   screen_type="wide"
 else
   screen_type="normal"
