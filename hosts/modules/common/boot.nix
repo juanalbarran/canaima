f3ace89c1653d5984f158a8323e76cc247d8ffa3
Juan Jesus <juanjesusalbarran@gmail.com>
2026-01-31 16:33:22 +0100

    Boot
    Added console config for the boot

--- hosts/modules/common/boot.nix
+++ hosts/modules/common/boot.nix
@@ -1,7 +1,20 @@
 # ./host/modules/common/boot.nix
 {
-  boot.loader = {
-    systemd-boot.enable = true;
-    efi.canTouchEfiVariables = true;
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+    consoleLogLevel = 0;
+    initrd.verbose = false;
   };
 }
