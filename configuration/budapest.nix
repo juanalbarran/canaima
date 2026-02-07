acb6295064cabce21b64581432902eb83f1d9896
Juan Jesus <juanjesusalbarran@gmail.com>
2026-02-07 18:10:24 +0100

    Budapest
    Removed home -manager configuration from budapest, it should go in the
    host part, in canaima.

--- configuration/budapest.nix
+++ configuration/budapest.nix
@@ -5,8 +5,6 @@
     ./../nixos/modules/ui/sway
     # fonts, boot, localization, networking, experimental-features
     ./../nixos/modules/common
-    # home-manager configuration
-    ./home-configuration/playa-el-agua
     # user
     ./../nixos/users/juan.nix
   ];
