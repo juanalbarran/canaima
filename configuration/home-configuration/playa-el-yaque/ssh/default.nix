cd4763ff9ca771b09bbf51077afd0205118f15d5
Juan Jesus Albarran Rodriguez <juanjesusalbarran@gmail.com>
2026-02-14 13:20:37 +0100

    ssh
    Added new ssh configuration
    Now the ssh configuration is handled by home-manager

--- configuration/home-configuration/playa-el-yaque/ssh/default.nix
+++ configuration/home-configuration/playa-el-yaque/ssh/default.nix
@@ -1,4 +1,15 @@
 # configuration/home-configuration/playa-el-yaque/ssh/default.nix
 {
   home.file.".ssh/playa-el-yaque.pub".source = ./playa-el-yaque.pub;
+  programs.ssh = {
+    enable = true;
+
+    matchBlocks = {
+      "github.com" = {
+        hostname = "github.com";
+        user = "git";
+        identityFile = "~/.ssh/playa-el-yaque";
+      };
+    };
+  };
 }
