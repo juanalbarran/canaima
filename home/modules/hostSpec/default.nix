7bbbd6bea3a9e54b691be6514879450a9b7fb3ac
Juan Jesus Albarran Rodriguez <juanjesusalbarran@gmail.com>
2026-02-20 18:05:26 +0100

    hostSpec
    Fixing typos
    Added the sskKeyName in hostSpec

--- home/modules/hostSpec/default.nix
+++ home/modules/hostSpec/default.nix
@@ -1,22 +1,21 @@
 # home/modules/hostSpec/default.nix
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}: {
+{lib, ...}: {
   options.hostSpec = {
     username = lib.mkOption {
       type = lib.types.str;
       description = "The username of the host";
     };
     hostname = lib.mkOption {
-      type = lib.type.str;
+      type = lib.types.str;
       description = "The hostname of the host";
     };
     email = lib.mkOption {
       type = lib.types.attrsOf lib.types.str;
       description = "The email of the user";
+    };
+    sshKeyName = lib.mkOption {
+      type = lib.types.str;
+      description = "The name of the ssh keys of the user";
     };
   };
 }
