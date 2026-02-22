85827364c818bb268f6193452a8d305e6d720460
Juan Jesus Albarran Rodriguez <juanjesusalbarran@gmail.com>
2026-02-22 08:08:43 +0100

    dwm
    Commenting some keybinds

--- nixos/modules/ui/dwm/config/config.h
+++ nixos/modules/ui/dwm/config/config.h
@@ -58,17 +58,17 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *projectscmd[] = {"projects", NULL};
-static const char *sysmenucmd[] = {"system-menu", NULL};
-static const char *bookmarkcmd[] = {"bookmarks", NULL};
+//static const char *projectscmd[] = {"projects", NULL};
+//static const char *sysmenucmd[] = {"system-menu", NULL};
+//static const char *bookmarkcmd[] = {"bookmarks", NULL};
 //static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = projectscmd } },
-	{ MODKEY,                       XK_d,      spawn,          {.v = sysmenucmd } },
-	{ MODKEY,                       XK_m,      spawn,          {.v = bookmarkcmd } },
-	{ MODKEY|ShiftMask,             XK_q,      spawn,          {.v = termcmd } },
+//{ MODKEY,                       XK_p,      spawn,          {.v = projectscmd } },
+//{ MODKEY,                       XK_d,      spawn,          {.v = sysmenucmd } },
+//{ MODKEY,                       XK_m,      spawn,          {.v = bookmarkcmd } },
+//{ MODKEY|ShiftMask,             XK_q,      spawn,          {.v = termcmd } },
 	{ MODKEY|ControlMask,           XK_b,      togglebar,      {0} },
 	{ MODKEY|ControlMask,           XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY|ControlMask,           XK_k,      focusstack,     {.i = -1 } },
@@ -78,7 +78,7 @@ static const Key keys[] = {
 	{ MODKEY|ControlMask,           XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY|ControlMask,           XK_Return, zoom,           {0} },
 	{ MODKEY|ControlMask,           XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,					              XK_c,      killclient,     {0} },
 	{ MODKEY|ControlMask,           XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY|ControlMask,           XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY|ControlMask,           XK_m,      setlayout,      {.v = &layouts[2]} },
