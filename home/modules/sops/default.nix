cc5ca854fbbd38382a69826c39bd86ca6cd09d62
Juan Jesus Albarran Rodriguez <juanjesusalbarran@gmail.com>
2026-02-23 18:05:51 +0100

    sops
    Added it more like a module

--- home/modules/sops/default.nix
+++ home/modules/sops/default.nix
@@ -8,22 +8,35 @@
 }: let
   secretsPath = toString inputs.secrets;
   sshKeyName = config.hostSpec.sshKeyName;
+  homePath = config.home.homeDirectory;
 in {
   imports = [
     sops-nix.homeManagerModules.sops
   ];
   sops = {
+    age.keyFile = "${homePath}/.config/sops/age/keys.txt";
     age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
     defaultSopsFile = "${secretsPath}/secrets.yaml";
     validateSopsFiles = false;
 
     secrets = {
       "private_keys/${sshKeyName}" = {
-        path = "/home/juan/.ssh/${sshKeyName}";
+        path = "${homePath}/.ssh/${sshKeyName}";
       };
       "access_tokens/github_token" = {};
     };
+    templates = {
+      "github-token" = {
+        path = "${homePath}/.config/access_tokens/github_token";
+        content = ''
+          access-tokens = github.com=${config.sops.placeholder."access_tokens/github_token"}
+        '';
+      };
+    };
   };
+  nix.extraOptions = ''
+    !include ${config.sops.templates."github-token".path}
+  '';
   home.packages = with pkgs; [
     sops
     age
