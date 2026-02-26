e42cbaeab1dba5e52f9b71a093b65a1baa80e0ac
Juan Jesus Albarran Rodriguez <juanjesusalbarran@gmail.com>
2026-02-26 16:23:36 +0100

    packages and programs
    Adding packages and programs to core module

---
+++ home/modules/core/packages.nix
@@ -0,0 +1,20 @@
+# home/modules/core/packages.nix
+{
+  pkgs,
+  kukenan,
+  ...
+}: let
+  kuks = kukenan.packages.${pkgs.system}.neovim.base;
+  kuks-web = kukenan.packages.${pkgs.system}.neovim.web;
+in {
+  home.packages = with pkgs; [
+    ripgrep
+    tree
+    gcc
+    # editor
+    kuks
+    kuks-web
+    # development
+    devenv
+  ];
+}
---
+++ home/modules/core/programs.nix
@@ -0,0 +1,38 @@
+# home/modules/core/programs.nix
+{config, ...}: let
+  fullname = config.hostSpec.fullname;
+in {
+  programs = {
+    home-manager.enable = true;
+    bat.enable = true;
+    yazi.enable = true;
+    lazygit.enable = true;
+    git = {
+      enable = true;
+      includes = [
+        {path = config.sops.templates."git-email".path;}
+      ];
+      settings = {
+        user = {
+          name = "${fullname}";
+        };
+      };
+    };
+    bash = {
+      enable = true;
+      shellAliases = {
+        vim = "nvim-web";
+        cat = "bat";
+        jvim = "nvim-java";
+        nvim = "nvim-max";
+      };
+      initExtra = ''
+        fastfetch
+      '';
+    };
+  };
+  home.sessionVariables = {
+    EDITOR = "nvim-base";
+    VISUAL = "nvim-base";
+  };
+}
