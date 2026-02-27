bbc9f92598f5512514420697fbd9db30d2175acc
Juan Jesus Albarran Rodriguez <juanjesusalbarran@gmail.com>
2026-02-27 09:11:54 +0100

    Updating default.nix

--- nixos/modules/ui/dwl/default.nix
+++ nixos/modules/ui/dwl/default.nix
@@ -3,18 +3,20 @@
   environment.systemPackages = with pkgs; [
     (dwl.overrideAttrs (oldAttrs: {
       src = ./config;
-      patches = (oldAttrs.patches or []) ++ [
-      (pkgs.fetchpatch {
-        name = "dwl-movestack";
-        url = "https://codeberg.org/dwl/dwl-patches/raw/branch/main/patches/movestack/movestack-0.7.patch";
-          hash = pkgs.lib.fakeHash;
-       })
-      (pkgs.fetchpatch {
-        name = "dwl-warpcursor";
-        url = "https://codeberg.org/dwl/dwl-patches/raw/branch/main/patches/warpcursor/warpcursor-0.7.patch";
-          hash = pkgs.lib.fakeHash;
-       })
-      ];
+      patches =
+        (oldAttrs.patches or [])
+        ++ [
+          (pkgs.fetchpatch {
+            name = "dwl-movestack";
+            url = "https://codeberg.org/dwl/dwl-patches/raw/branch/main/patches/movestack/movestack-0.7.patch";
+            hash = pkgs.lib.fakeHash;
+          })
+          (pkgs.fetchpatch {
+            name = "dwl-warpcursor";
+            url = "https://codeberg.org/dwl/dwl-patches/raw/branch/main/patches/warpcursor/warpcursor-0.7.patch";
+            hash = pkgs.lib.fakeHash;
+          })
+        ];
       postPatch = ''
         cp ${./config/config.h} config.h
       '';
@@ -22,17 +24,19 @@
     wl-clipboard
     wlr-randr
   ];
-  services = { 
-    displayManager = { 
+  services = {
+    displayManager = {
       ly.enable = true;
       sessionPackages = [
-        (pkgs.writeTextDir "share/wayland-sessions/dwl.desktop" ''
-          [Desktop Entry]
-          Name=dwl
-          Comment=Dynamic window manager for Wayland
-          Exec=dwl
-          Type=Application
-        '')
+        ((pkgs.writeTextDir "share/wayland-sessions/dwl.desktop" ''
+            [Desktop Entry]
+            Name=dwl
+            Comment=Dynamic window manager for Wayland
+            Exec=dwl
+            Type=Application
+          '').overrideAttrs (old: {
+            passthru.providedSessions = ["dwl"];
+          }))
       ];
     };
   };
