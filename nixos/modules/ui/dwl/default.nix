5e88d01ec9b49e2bf0463aba91ea69b09b95e2a7
Juan Jesus Albarran Rodriguez <juanjesusalbarran@gmail.com>
2026-02-27 16:35:19 +0100

    dwl
    Changed config, now i'm using nixos own module
    Using the custom config.h for configuration

--- nixos/modules/ui/dwl/default.nix
+++ nixos/modules/ui/dwl/default.nix
@@ -1,45 +1,41 @@
 # nixos/modules/ui/dwl/default.nix
 {pkgs, ...}: {
+  programs.dwl = {
+    enable = true;
+    package =
+      (pkgs.dwl.override {
+        configH = ./config.h;
+      }).overrideAttrs (oldAttrs: {
+        # src = ./config;
+
+        patches =
+          (oldAttrs.patches or [])
+          ++ [
+            (pkgs.fetchpatch {
+              name = "dwl-movestack";
+              url = "https://codeberg.org/dwl/dwl-patches/raw/branch/main/patches/movestack/movestack.patch";
+              hash = "sha256-/Ac7oQyZNVPqGiNDn0y94arN0cz98Ie1nKkQIX27bZo=";
+            })
+            (pkgs.fetchpatch {
+              name = "dwl-warpcursor";
+              url = "https://codeberg.org/dwl/dwl-patches/raw/branch/main/patches/warpcursor/warpcursor.patch";
+              hash = "sha256-0AGMq507WmW2QJW02u6eJDuQmGBAiVPbEw79npwqEDU=";
+            })
+          ];
+      });
+  };
+
   environment.systemPackages = with pkgs; [
-    (dwl.overrideAttrs (oldAttrs: {
-      src = ./config;
-      patches =
-        (oldAttrs.patches or [])
-        ++ [
-          (pkgs.fetchpatch {
-            name = "dwl-movestack";
-            url = "https://codeberg.org/dwl/dwl-patches/raw/branch/main/patches/movestack/movestack.patch";
-            hash = "sha256-/Ac7oQyZNVPqGiNDn0y94arN0cz98Ie1nKkQIX27bZo=";
-          })
-          (pkgs.fetchpatch {
-            name = "dwl-warpcursor";
-            url = "https://codeberg.org/dwl/dwl-patches/raw/branch/main/patches/warpcursor/warpcursor.patch";
-            hash = "sha256-0AGMq507WmW2QJW02u6eJDuQmGBAiVPbEw79npwqEDU=";
-          })
-        ];
-      postPatch = ''
-        cp ${./config/config.h} config.h
-      '';
-    }))
     wl-clipboard
     wlr-randr
   ];
+
   services = {
     displayManager = {
       ly.enable = true;
-      sessionPackages = [
-        ((pkgs.writeTextDir "share/wayland-sessions/dwl.desktop" ''
-            [Desktop Entry]
-            Name=dwl
-            Comment=Dynamic window manager for Wayland
-            Exec=dwl
-            Type=Application
-          '').overrideAttrs (old: {
-            passthru.providedSessions = ["dwl"];
-          }))
-      ];
     };
   };
+
   programs.xwayland.enable = true;
   programs.dconf.enable = true;
   security.polkit.enable = true;
