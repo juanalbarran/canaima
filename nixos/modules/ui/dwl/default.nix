bd084ef9ed31d9ef166672abf4ec5b8c043cbc6e
Juan Jesus Albarran Rodriguez <juanjesusalbarran@gmail.com>
2026-02-27 20:15:07 +0100

    dwl
    gettin rid of the startup message
    Environment variable $DISPLAY not set, ignoring
    Environment variable $XAUTHORITY not set, ignoring

--- nixos/modules/ui/dwl/default.nix
+++ nixos/modules/ui/dwl/default.nix
@@ -1,28 +1,40 @@
 # nixos/modules/ui/dwl/default.nix
-{pkgs, ...}: {
+{pkgs, ...}: let
+  caracasDwl =
+    (pkgs.dwl.override {
+      configH = ./config.h;
+    }).overrideAttrs (oldAttrs: {
+      patches =
+        (oldAttrs.patches or [])
+        ++ [
+          (pkgs.fetchpatch {
+            name = "dwl-movestack";
+            url = "https://codeberg.org/dwl/dwl-patches/raw/branch/main/patches/movestack/movestack.patch";
+            hash = "sha256-/Ac7oQyZNVPqGiNDn0y94arN0cz98Ie1nKkQIX27bZo=";
+          })
+          (pkgs.fetchpatch {
+            name = "dwl-warpcursor";
+            url = "https://codeberg.org/dwl/dwl-patches/raw/branch/main/patches/warpcursor/warpcursor.patch";
+            hash = "sha256-0AGMq507WmW2QJW02u6eJDuQmGBAiVPbEw79npwqEDU=";
+          })
+        ];
+    });
+in {
   programs.dwl = {
     enable = true;
-    package =
-      (pkgs.dwl.override {
-        configH = ./config.h;
-      }).overrideAttrs (oldAttrs: {
-        # src = ./config;
+    package = caracasDwl;
+    extraSessionCommands = ''
+      unset DISPLAY
+      unset XAUTHORITY
+      systemctl --user import-environment WAYLAND_DISPLAY >/dev/null 2>&1
+      systemctl --user start dwl-session.target
+      exec ${caracasDwl}/bin/dwl
+    '';
+  };
 
-        patches =
-          (oldAttrs.patches or [])
-          ++ [
-            (pkgs.fetchpatch {
-              name = "dwl-movestack";
-              url = "https://codeberg.org/dwl/dwl-patches/raw/branch/main/patches/movestack/movestack.patch";
-              hash = "sha256-/Ac7oQyZNVPqGiNDn0y94arN0cz98Ie1nKkQIX27bZo=";
-            })
-            (pkgs.fetchpatch {
-              name = "dwl-warpcursor";
-              url = "https://codeberg.org/dwl/dwl-patches/raw/branch/main/patches/warpcursor/warpcursor.patch";
-              hash = "sha256-0AGMq507WmW2QJW02u6eJDuQmGBAiVPbEw79npwqEDU=";
-            })
-          ];
-      });
+  systemd.services.display-manager.environment = {
+    DISPLAY = ":0";
+    XAUTHORITY = "/tmp/xauth";
   };
 
   environment.systemPackages = with pkgs; [
@@ -30,13 +42,8 @@
     wlr-randr
   ];
 
-  services = {
-    displayManager = {
-      ly.enable = true;
-    };
-  };
+  services.displayManager.ly.enable = true;
 
-  programs.xwayland.enable = true;
   programs.dconf.enable = true;
   security.polkit.enable = true;
   hardware.graphics.enable = true;
