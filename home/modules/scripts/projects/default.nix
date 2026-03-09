0e5fa97a8b1697d31d26a36be4925c6eb2a1b620
Juan Jesus Albarran Rodriguez <juanjesusalbarran@gmail.com>
2026-03-09 17:31:33 +0100

    Scripts
    Added the project script
    Now the project script will open a project with the terminal and menu
    defined in the hostSpec of the user

--- home/modules/scripts/projects/default.nix
+++ home/modules/scripts/projects/default.nix
@@ -1,8 +1,21 @@
 # home/modules/scripts/projects/projects.nix
-{pkgs, ...}: let
+{
+  pkgs,
+  config,
+  ...
+}: let
+  terminal = config.hostSpec.terminal;
+  terminalAppId = config.hostSpec.terminalAppId;
+  menu = config.hostSpec.menu;
   projects-base = pkgs.writeShellScriptBin "projects-base" (builtins.readFile ./projects.sh);
   projects = pkgs.writeShellScriptBin "projects" ''
-    exec ${projects-base}/bin/projects-base -c "wmenu -p 'Projects:'" -t foot
+    if [ -f "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh" ]; then
+      source "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+    elif [ -f "/etc/profiles/per-user/$USER/etc/profile.d/hm-session-vars.sh" ]; then
+      source "/etc/profiles/per-user/$USER/etc/profile.d/hm-session-vars.sh"
+    fi
+    export BEMENU_BACKEND=wayland
+    exec ${projects-base}/bin/projects-base ${terminal} ${terminalAppId} ${menu}
   '';
 in {
   home.packages = [
