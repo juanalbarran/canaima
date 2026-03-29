05e460538650f8f39d0ad0a5882db0d4c8a7ef8e
Juan Jesus <juanjesusalbarran@gmail.com>
2026-03-29 10:16:14 +0200

    Bookmarks
    Fixing bookemarks script

--- home/modules/scripts/bookmarks/default.nix
+++ home/modules/scripts/bookmarks/default.nix
@@ -5,18 +5,23 @@
   ...
 }: let
   menu = config.hostSpec.menu;
+  isNixOS = config.hostSpec.isNixOS;
+  username = config.hostSpec.username;
+  envPath = 
+    if isNixOS 
+    then "/etc/profiles/per-user/${username}/etc/profile.d/hm-session-vars.sh"
+    else "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh";
 
   # Read the bash script below
   bookmarks-base = pkgs.writeShellScriptBin "bookmarks-base" (builtins.readFile ./bookmarks.sh);
 
   # Wrapper to source variables and execute the base script with the menu command
   bookmarks = pkgs.writeShellScriptBin "bookmarks" ''
-    if [ -f "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh" ]; then
-      source "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
-    elif [ -f "/etc/profiles/per-user/$USER/etc/profile.d/hm-session-vars.sh" ]; then
-      source "/etc/profiles/per-user/$USER/etc/profile.d/hm-session-vars.sh"
-    fi
 
+    if [ -f "${envPath}" ]; then
+      source "${envPath}"
+    fi
+    
     export BEMENU_BACKEND=wayland
 
     # Pass the menu string from hostSpec as arguments to the base script
@@ -26,4 +31,11 @@ in {
   home.packages = [
     bookmarks
   ];
+  xdg.configFile = {
+    "bookmarks/chill.txt".source = ./../../../assets/bookmarks/chill.txt;
+    "bookmarks/work.txt".source = ./../../../assets/bookmarks/work.txt;
+    "bookmarks/code.txt".source = ./../../../assets/bookmarks/code.txt;
+    "bookmarks/dotfiles.txt".source = ./../../../assets/bookmarks/dotfiles.txt;
+    "bookmarks/blogs.txt".source = ./../../../assets/bookmarks/blogs.txt;
+  };
 }
